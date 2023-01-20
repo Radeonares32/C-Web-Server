333c2d04674d98b226c05f627f02796f843913cf
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-20 20:02:39 +0300

    library added & main func created

---
+++ http_client.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv){
+    
+}
