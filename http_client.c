e03ffa045d08ad186c840240a929d22829111d2a
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-20 20:11:18 +0300

    http client server finish

--- http_client.c
+++ http_client.c
@@ -7,24 +7,31 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main(int argc, char *argv){
+int main(int argc, char *argv[])
+{
     char *address;
     address = argv[1];
-    
-    int client_socket;
-    client_socket = socket(AF_INET,SOCK_STREAM,0);
 
-    //connect to an address
+    int client_socket;
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // connect to an address
 
     struct sockaddr_in remote_address;
     remote_address.sin_family = AF_INET;
     remote_address.sin_port = htons(80);
-    inet_aton(address,&remote_address.sin_addr.s_addr);
+    inet_aton(address, &remote_address.sin_addr.s_addr);
 
-    connect(client_socket,(struct sockaddr *) &remote_address,sizeof(remote_address));
+    connect(client_socket, (struct sockaddr *)&remote_address, sizeof(remote_address));
 
     char request[] = "GET / HTTP/1.1\r\n\r\n";
     char response[4096];
 
-    send(client_socket,request,sizeof(request),0);
+    send(client_socket, request, sizeof(request), 0);
+    recv(client_socket, &response, sizeof(response), 0);
+
+    printf("response from the serer : %s\n",response);
+    close(client_socket);
+
+    return 0;
 }
