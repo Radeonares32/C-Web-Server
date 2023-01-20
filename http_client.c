812c96de5b4cc76a581396cce82a565ce7f95e04
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-20 20:09:08 +0300

    request and response created varr

--- http_client.c
+++ http_client.c
@@ -20,4 +20,11 @@ int main(int argc, char *argv){
     remote_address.sin_family = AF_INET;
     remote_address.sin_port = htons(80);
     inet_aton(address,&remote_address.sin_addr.s_addr);
+
+    connect(client_socket,(struct sockaddr *) &remote_address,sizeof(remote_address));
+
+    char request[] = "GET / HTTP/1.1\r\n\r\n";
+    char response[4096];
+
+    send(client_socket,request,sizeof(request),0);
 }
